884125a5903a095e01b36fb5fb392104a28a62f5
Emmanuel Amanga <emmanuelamanga2013@gmail.com>
2021-03-15 10:07:17 +0300

    print kpi

--- resources/views/structureManager/structure_kpis.blade.php
+++ resources/views/structureManager/structure_kpis.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="card">
     <div class="card-header h4 bg-dark text-uppercase">
-        {{$structure->name}} {{$structure_type}} KPI<small>s</small>
+      <div id="title">{{$structure->name}} {{$structure_type}} KPI<small>s</small></div>
         <a href="{{url()->previous()}}" class="float-right m-1 btn-xs btn-info">
             <i class="fas  nav-icon"></i> Back
         </a>
@@ -29,7 +29,7 @@
                 <tr id="{{$kpi->code}}" class="record">
                     <td> {{$kpi->code}}</td>
                     @php
-                        $kp = new App\KPI;
+                    $kp = new App\KPI;
                     @endphp
                     <td> {{$kp->kpiPerspective($kpi->perspective)->perspective}}</td>
                     <td id="kpi"> {{$kpi->kpi}}</td>
@@ -38,24 +38,24 @@
                         @if ($kpi->kpi_type == 'Tasked')
                         <a href="{{URL::signedRoute('kpiTasks',[$kpi->id])}}"
                             class="btn btn-xs btn-outline-primary float-left m-1">
-                            {{-- <i class="badge badge-info">{{$kpi->tasks->count()}}</i> Tasks --}}
+                            <i class="badge badge-info">{{--$kpi->tasks->count()--}}</i> Tasks
                         </a>
                         @endif
-                        @can('kpi-edit')
+                        {{-- @can('kpi-edit') --}}
                         <a href="{{URL::signedRoute('kpi.edit',[$kpi->id])}}"
                             class="btn btn-xs btn-outline-warning float-left mr-1">
                             {{-- <i class="fa fa-edit"></i> --}}
                             Edit
                         </a>
-                        @endcan
-                        @can('kpi-delete')
+                        {{-- @endcan --}}
+                        {{-- @can('kpi-delete') --}}
                         <form action="{{route('kpi.destroy',[$kpi->id])}}" method="post">
                             @csrf
                             @method('DELETE')
                             <button type="submit" onclick="return confirm('Are You Sure ?')"
                                 class="btn btn-xs btn-outline-danger float-left mr-1">Trash</button>
                         </form>
-                        @endcan
+                        {{-- @endcan --}}
                     </td>
                 </tr>
                 @empty
@@ -81,11 +81,65 @@
 <script>
     //
 $(function () {
+    // $("#kpiTable").DataTable({
+    // "responsive": true, "lengthChange": true, "autoWidth": false,'ordering': true,
+    // "buttons": ["copy", "csv", "excel", "pdf", "print"]
+    // }).buttons().container().appendTo('#kpiTable_wrapper .col-md-6:eq(0)');
+    // console.log($('.main-footer strong').text())
+        // console.log($('#title').text())
+        var title = $('#title').text();
+    // Append a caption to the table before the DataTables initialisation
+    $('#kpiTable').append('<caption style="caption-side: bottom">'+$('.main-footer strong').text()+' All rights reserved.</caption>');
+
     $("#kpiTable").DataTable({
     "responsive": true, "lengthChange": true, "autoWidth": false,'ordering': true,
-    // "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
+    "buttons": [
+            {
+                extend: 'copyHtml5',
+                messageTop: title,
+                messageBottom: 'The information in this table is copyright to Sirius Cybernetics Corp.',
+                exportOptions: {
+                columns: [ 0, 1, 2, 3]
+            }
+        },
+            {
+                extend: 'excelHtml5',
+                messageTop: title,
+                messageBottom: 'The information in this table is copyright to Sirius Cybernetics Corp.',
+                exportOptions: {
+                columns: [ 0, 1, 2, 3]
+            }
+        },
+            {
+                extend: 'pdfHtml5',
+                messageTop: title,
+                messageBottom: 'The information in this table is copyright to Sirius Cybernetics Corp.',
+                exportOptions: {
+                    columns: [ 0, 1, 2, 3]
+                }
+        },
+            {
+                extend: 'print',
+                messageTop: title,
+                messageBottom: 'The information in this table is copyright to Sirius Cybernetics Corp.',
+                exportOptions: {
+                    columns: ':visible'
+                },
+                // customize: function ( win ) {
+                //     $(win.document.body)
+                //         .css( 'font-size', '10pt' )
+                //         .prepend(
+                //         '<img src="http://127.0.0.1:8000/css/dist/img/logo_2.jpeg" style="position:absolute; top:0; left:0;" />'
+                //     );
+                //     $(win.document.body).find( 'table' )
+                //     .addClass( 'compact' )
+                //     .css( 'font-size', 'inherit' );
+                // }
+            },
+            "colvis"
+    ]
     }).buttons().container().appendTo('#kpiTable_wrapper .col-md-6:eq(0)');
-
+//  is a button
     })
 </script>
 @endsection
--- resources/views/structureManager/structure_members.blade.php
+++ resources/views/structureManager/structure_members.blade.php
@@ -21,25 +21,40 @@
             <th>Section</th>
             <th>Sub-Section</th>
             <th width="350px">Action</th>
-        </thead>
-        @foreach ($data as $key => $user)
+        </thead> @php
+                $structure = new App\User;
+            @endphp
+        @foreach ($structure->hydrate($data) as $key => $user)
         <tr>
             <td>{{ ++$key }}</td>
             <td>
                 {{$user->first_name}} {{$user->middle_name}} {{$user->last_name}}
                 {{-- {{ $user->fullName($user->id) ?? ''}} --}}
             </td>
+
             <td>{{ $user->email }}</td>
-            <td>{{ $user->department_id ?? ''}}</td>
-            <td>{{ $user->section_id ?? ''}}</td>
-            <td>{{ $user->sub_section_id ?? ''}}</td>
-            <td class="" style="display: inline">
+            <td>{{ $user->department->department_name ?? ''}}</td>
+            <td>{{ $user->section->section_name ?? ''}}</td>
+            <td>{{ $user->subsection->sub_section_name ?? ''}}</td>
+            <td class="p-1" style="display: inline">
+
                 <a class="btn btn-xs btn-outline-info float-left" href="{{ route('users.show',$user->id) }}">Show</a>
-                <a class="btn btn-xs btn-outline-primary float-left" href="{{ route('users.edit',$user->id) }}">Edit</a>
-                {!! Form::open(['method' => 'DELETE','route' => ['users.destroy', $user->id],'style'=>'',
+                @can('users-edit')
+<a class="btn btn-xs btn-outline-primary float-left m-1" href="{{ route('users.edit',$user->id) }}">Edit</a>
+                @endcan
+                {{-- {!! Form::open(['method' => 'DELETE','route' => ['users.destroy', $user->id],'style'=>'',
                 'class'=>'float-left']) !!}
                 {!! Form::submit('Trash', ['class' => 'btn btn-xs btn-outline-danger', 'onclick']) !!}
-                {!! Form::close() !!}
+                {!! Form::close() !!} --}}
+                @can('users-delete')
+                <form action="{{route('users.destroy',[$user->id])}}" method="post" class="float-left">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" onclick="return confirm('Are You Sure ?')"
+                        class="btn btn-xs btn-outline-danger">Trash</button>
+                </form>
+                @endcan
+
             </td>
         </tr>
         @endforeach
